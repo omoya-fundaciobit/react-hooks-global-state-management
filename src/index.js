272a03f5f23079d500c2a82c19574c91382c3061
omoya_fbit <omoya@fundaciobit.org>
2020-11-01 23:53:37 +0100

    Removed unused code

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import store from "./store/config/storeConfig";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
--- src/store/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import contactsReducer from "./contactsReducer";
-
-const rootReducer = combineReducers({
-  contactsState: contactsReducer,
-});
-
-export default rootReducer;
